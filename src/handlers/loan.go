5bafba02860d82c4e942ba267f56cc8110378e4b
Daniel Befekadu <145967090+DanielBefekadu@users.noreply.github.com>
2026-01-31 00:32:01 -0800

    Update loan.go

--- src/handlers/loan.go
+++ src/handlers/loan.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 	"strconv"
 
-	"Ci-CD-loan/services"
+	"github.com/DanielBefekadu/Ci-CD-loan/services"
 )
 
 func RepaymentScheduleHandler(w http.ResponseWriter, r *http.Request) {
